5f46ed4ca41bc557f13765af0dfe2f4991a72783
Jorge Alcaras <jorge.alcaras@cceo.com.mx>
2024-03-12 22:09:49 -0600

    Feature (News App): :sparkles: Show articles by category.

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,24 +1,25 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app/src/services/news_service.dart';
 import 'package:news_app/src/models/category_model.dart';
+import 'package:news_app/src/services/news_service.dart';
+import 'package:news_app/src/widgets/list_news.dart';
 import 'package:provider/provider.dart';
-
 
 class Tab2Page extends StatelessWidget {
   const Tab2Page({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final categories = Provider.of<NewsService>(context).categories;
-    return const SafeArea(
+    final news = Provider.of<NewsService>(context).articlesCategorySelected;
+
+    return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-           Expanded(child: _CategoryList())
-       ]
-        ),
+        body: Column(children: [
+          _CategoryList(),
+          Expanded(
+            child: ListNews(news: news),
+          )
+        ]),
       ),
     );
   }
@@ -29,64 +30,60 @@ class _CategoryList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final categories = Provider.of<NewsService>(context).categories;
     final newsService = Provider.of<NewsService>(context);
 
-    return  ListView.builder(
-      physics: const BouncingScrollPhysics(),
+    return Container(
+      width: double.infinity,
+      height: 80,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
         itemCount: categories.length,
         itemBuilder: (BuildContext context, int index) {
-
-        final categoryName = categories[index].name;
+          final categoryName = categories[index].name;
 
           return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      _CategoryButton(categories[index]),
-                      Text('${categoryName[0].toUpperCase()}${categoryName.substring(1)}')
-                    ],
-                ),
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                _CategoryButton(categories[index]),
+                Text(
+                    '${categoryName[0].toUpperCase()}${categoryName.substring(1)}')
+              ],
+            ),
           );
         },
+      ),
     );
   }
 }
 
 class _CategoryButton extends StatelessWidget {
-
   final CategoryNews category;
 
   const _CategoryButton(this.category);
 
   @override
   Widget build(BuildContext context) {
-
     final newsService = Provider.of<NewsService>(context);
 
     return GestureDetector(
       onTap: () {
         print(category.name);
         final newsService = Provider.of<NewsService>(context, listen: false);
-        newsService.selectedCategory = category.name;
+        newsService.setSelectedCategory = category.name;
       },
       child: Container(
         width: 40,
         height: 40,
         margin: const EdgeInsets.symmetric(horizontal: 10),
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.black12
-        ),
-        child: Icon(
-          category.icon,
-          color: (newsService.selectedCategory == category.name)?
-          Colors.white
-          : Colors.black26
-
-        ),
+        decoration:
+            BoxDecoration(shape: BoxShape.circle, color: Colors.black12),
+        child: Icon(category.icon,
+            color: (newsService.selectedCategory == category.name)
+                ? Colors.white
+                : Colors.black26),
       ),
     );
   }
--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:news_app/src/models/news_models.dart';
-import 'package:news_app/src/models/category_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:news_app/src/models/category_model.dart';
+import 'package:news_app/src/models/news_models.dart';
 
 final _URL_NEWS = 'https://newsapi.org/v2';
 final _APIKEY = '244540bb56ea453da0b3aae761e1e344';
 
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
-  String selectedCategory = 'business';
+  String _selectedCategory = 'business';
 
   List<CategoryNews> categories = [
     CategoryNews(Icons.business, 'business'),
@@ -22,24 +22,26 @@ class NewsService with ChangeNotifier {
 
   Map<String, List<Article>> categoryArticles = {};
 
-
   NewsService() {
     getTopHeadlines();
 
     for (var item in categories) {
       categoryArticles[item.name] = [];
-
     }
+
+    getArticlesByCategory(_selectedCategory);
   }
 
-  get getSelectedCategory => selectedCategory;
+  get selectedCategory => _selectedCategory;
 
   set setSelectedCategory(String value) {
-    selectedCategory = value;
+    _selectedCategory = value;
     getArticlesByCategory(value);
     notifyListeners();
   }
 
+  List<Article> get articlesCategorySelected =>
+      categoryArticles[_selectedCategory] ?? [];
 
   getTopHeadlines() async {
     final url = '$_URL_NEWS/top-headlines?apiKey=$_APIKEY&country=mx';
@@ -48,15 +50,15 @@ class NewsService with ChangeNotifier {
     final newsResponse = NewResponse.fromJson(answer.body);
 
     headlines.addAll(newsResponse.articles);
-    print('Headlines: ${headlines.length}');//This is for debugging
     notifyListeners();
   }
 
-  getArticlesByCategory (String category) async {
+  getArticlesByCategory(String category) async {
     if (categoryArticles[category]!.isNotEmpty) {
       return categoryArticles[category];
     }
-    final url = '$_URL_NEWS/top-headlines?apiKey=$_APIKEY&country=mx&category=$category';
+    final url =
+        '$_URL_NEWS/top-headlines?apiKey=$_APIKEY&country=mx&category=$category';
     final answer = await http.get(Uri.parse(url));
 
     final newsResponse = NewResponse.fromJson(answer.body);
